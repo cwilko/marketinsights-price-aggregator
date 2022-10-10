d2b4ceb59999dffda48fdab0f76e6460d007d4bb
cwilko <chris.wilkinson@uk.ibm.com>
2022-10-10 21:17:57 +0100

    Nightly cronjob to get data

--- jobs/nightly/app.py
+++ jobs/nightly/app.py
@@ -32,7 +32,7 @@ def appendOptionChainPrices(mds, ds_file):
 
                 # Get todays prices from optionChain
                 print("Requesting " + optionChain["name"])
-                optionData = dataConnector.getOptions(optionChain, appendUnderlying=False, debug=True)
+                optionData = dataConnector.getOptions(optionChain, appendUnderlying=False, debug=False)
 
                 if optionData is not None:
 
