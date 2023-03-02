ad53cc2dfa89e5f756a48544da1c455a6ff8a5c8
cwilko <chris.wilkinson@uk.ibm.com>
2023-03-02 12:50:56 +0000

    Aggregation performance improvement

--- MIPriceAggregator/api/aggregator.py
+++ MIPriceAggregator/api/aggregator.py
@@ -37,69 +37,6 @@ class MarketDataAggregator:
         for datasource in config:
             self.datasources[datasource["ID"]] = MarketDataSource(connectorClass=datasource["class"], connName=datasource["ID"], tz=datasource["timezone"], options=datasource["opts"])
 
-    def getTestData(self, mkts=None, sample_unit=None, start="1979-01-01", end="2050-01-01", records=200, aggregate=True, debug=False):
-
-        if aggregate:
-            resultData = pd.DataFrame(pd.DataFrame(index=pd.MultiIndex(levels=[[], []], codes=[[], []], names=[u'mID', u'Date_Time'])))
-        else:
-            resultData = pd.DataFrame(pd.DataFrame(index=pd.MultiIndex(levels=[[], [], []], codes=[[], [], []], names=[u'mID', u'sID', u'Date_Time'])))
-
-        for datasource in self.config:
-
-            mds = self.datasources[datasource["ID"]]
-
-            markets = [market for market in datasource["markets"] if ((not mkts) or (market["ID"] in mkts))]
-
-            data = mds.getData(markets, start, end, records, debug)
-
-            for market in tqdm(markets):
-
-                marketData = pd.DataFrame(pd.DataFrame(index=pd.MultiIndex(levels=[[], []], codes=[[], []], names=[u'Date_Time', u'mID'])))
-
-                for source in market["sources"]:
-
-                    tsData = data[data.index.get_level_values('ID') == source["ID"]]
-
-                    if not tsData.empty:
-
-                        tsData = tsData \
-                            .reset_index() \
-                            .set_index("Date_Time")[["Open", "High", "Low", "Close"]]
-
-                        if sample_unit:  # Perform resampling
-
-                            # 28/6/21 Move this to before data is saved for performance reasons
-                            # Resample all to dataset sample unit (to introduce nans in all missing periods)
-                            tsData = ppl.resample(tsData, source["sample_unit"], debug)
-
-                            # Resample to the requested unit
-                            tsData = ppl.resample(tsData, sample_unit, debug)
-
-                            # 06/06/18
-                            # Remove NaNs and resample again, to remove partial NaN entries before merging
-                            tsData = ppl.removeNaNs(tsData)
-                            #tsData = ppl.resample(tsData, sample_unit, debug)
-
-                        # Reset IDs
-                        tsData["mID"] = market["ID"]
-
-                        if aggregate:
-                            tsData.set_index('mID', append=True, inplace=True)
-                            marketData = ppl.merge(tsData, marketData)
-                        else:
-                            tsData["sID"] = source["ID"]
-                            tsData.set_index(['mID', 'sID'], append=True, inplace=True)
-                            resultData = ppl.merge(tsData, resultData)
-
-                if aggregate and not marketData.empty:
-                    resultData = ppl.merge(marketData, resultData)
-
-        # if aggregate:
-        #    resultData.set_index('sID', append=True, inplace=True)
-        #    resultData = resultData.droplevel("sID")
-
-        return resultData.sort_index()
-
     def getData(self, mkts=None, sample_unit=None, start="1979-01-01", end="2050-01-01", records=200, aggregate=True, debug=False):
 
         tsDatalist = []
